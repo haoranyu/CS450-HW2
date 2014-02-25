0b03b90da6eca1ace11963daf188e3eea0ea2516
haoranyu <haoran_yu@hotmail.com>
2014-02-24 22:09:21 -0600

    Problem4abc

--- problem4_b.py
+++ problem4_b.py
@@ -9,29 +9,29 @@ def randV(seed, m):
 	x = np.random.randn(m, 1)
 	return x
 
-def invIter( A, x, sigma):
+def RQIter( A, x):
 	m, n = A.shape
-	As = A - sigma * np.eye(m)
+	I = np.eye(m)
 	k = 0
 	eps = 10e-12
 	delx = 1
 	while (delx > eps):
 		k+=1
-		u = x / la.norm(x)
-		x = la.solve(As,u)
+		u = x/la.norm(x)
+		lam = np.dot(u.T,np.dot(A,u))
+		x = la.solve(A-lam*I,u)
 		delx = la.norm(abs(u) - abs(x / la.norm(x)))
-		lam = np.dot(u.T,x)
-
-	lam = 1. / lam + sigma
-	x	= x / la.norm(x)
-	return lam, x, k 
+	u = x/la.norm(x)
+	lam = np.dot(u.T,np.dot(A,u))
+	x	= x/la.norm(x,2)
+	return lam, x, k
 
 A = np.array([[6,2,1], [2,3,1], [1,1,1]])
 m, n = A.shape
 
 for ra in range(10):
 	x = randV(2, m)
-	eigValue, eigVector, time = invIter(np.copy(A), np.copy(x), 2)
+	eigValue, eigVector, time = RQIter(np.copy(A), np.copy(x))
 	print "eigenvalue: %g" % eigValue
 	print "eigenvector:"
 	print eigVector
